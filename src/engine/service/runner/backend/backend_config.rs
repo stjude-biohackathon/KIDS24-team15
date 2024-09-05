9b817db1dbc12087bae7972358ccf5028cb5090d
Andrew Frantz <andrew.frantz@stjude.org>
2024-09-05 14:56:45 -0400

    feat: config placeholders (#8)
    * Create basic loading and submitting for Generic Backends within config
    
    Once a backend trait exists, this will be moved to GenericBackend's submit implementation, but for now I just wanted a good way of testing
    
    * Use .entry to add default parameters to a submit unless those parameters are already set
    
    * feat: substitute_placeholders func
    
    * feat: lsf test
    
    * Update backend_config.rs
    
    * Update backend_config.rs
    
    * revise: make placeholder syntax same as WDL
    
    * fix: apply new syntax to other TOMLs
    
    ---------
    
    Co-authored-by: Braden Everson <bradeneverson@gmail.com>

--- src/engine/service/runner/backend/backend_config.rs
+++ src/engine/service/runner/backend/backend_config.rs
@@ -7,6 +7,21 @@ use std::{
 
 use serde::{Deserialize, Serialize};
 
+/// The left placeholder for the backend config
+const LEFT_PLACEHOLDER: &str = "~{";
+/// The right placeholder for the backend config
+const RIGHT_PLACEHOLDER: &str = "}";
+
+/// Substitutes placeholders in a string with values from a hashmap
+fn substitute_placeholders(s: &str, substitutions: &HashMap<String, String>) -> String {
+    let mut result = s.to_string();
+    for (key, value) in substitutions {
+        let placeholder_key = format!("{}{}{}", LEFT_PLACEHOLDER, key, RIGHT_PLACEHOLDER);
+        result = result.replace(&placeholder_key, value);
+    }
+    result
+}
+
 /// Configuration for an arbitrary backend
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct BackendConfig {
@@ -21,39 +36,31 @@ pub struct BackendConfig {
     /// The default ram if present
     #[serde(rename = "default-ram", default)]
     pub default_ram: Option<u32>,
+    /// The runtime attributes for the backend
+    pub runtime_attrs: Option<HashMap<String, String>>,
 }
 
 impl BackendConfig {
     /// Submits a backend based on its config. Likely this method will be removed and the branch for Generic will be moved to GenericBackend's submit method.
     /// Instead of this method we should have a to_backend() method or something similar that creates a Box<dyn Backend> based on config.
-    pub fn submit(
-        &self,
-        replacements: &mut HashMap<String, String>,
-        left_placeholder: &str,
-        right_placeholder: &str,
-    ) -> Option<Output> {
+    pub fn submit(&self, substitutions: &mut HashMap<String, String>) -> Option<Output> {
         // Replace default flags only if it isn't already set
 
         if let Some(cpu) = self.default_cpu {
-            replacements
+            substitutions
                 .entry("cpu".to_string())
                 .or_insert(cpu.to_string());
         }
 
         if let Some(ram) = self.default_ram {
-            replacements
+            substitutions
                 .entry("ram".to_string())
                 .or_insert(ram.to_string());
         }
 
         match &self.kind {
             BackendType::Generic(generic) => {
-                let mut command_str = generic.command.clone();
-                for (key, value) in replacements {
-                    let placeholder_key =
-                        format!("{}{}{}", left_placeholder, key, right_placeholder);
-                    command_str = command_str.replace(&placeholder_key, value);
-                }
+                let command_str = substitute_placeholders(&generic.command, substitutions);
 
                 let output = Command::new("sh")
                     .arg("-c")
@@ -102,11 +109,11 @@ mod tests {
         let config = Config::load_from_file("configs/generic_simple.toml")
             .expect("Load from example config");
         let backend = &config.backends[0];
-        let mut replacements = HashMap::new();
-        replacements.insert("name".to_string(), "Kids24".to_string());
+        let mut substitutions = HashMap::new();
+        substitutions.insert("name".to_string(), "Kids24".to_string());
 
         let output = backend
-            .submit(&mut replacements, "${", "}")
+            .submit(&mut substitutions)
             .expect("Get output from generic backend");
         assert_eq!(output.stdout, b"Hello Kids24\n");
     }
@@ -116,10 +123,10 @@ mod tests {
         let config = Config::load_from_file("configs/generic_simple.toml")
             .expect("Load from example config");
         let backend = &config.backends[1];
-        let mut replacements = HashMap::new();
+        let mut substitutions = HashMap::new();
 
         let output = backend
-            .submit(&mut replacements, "${", "}")
+            .submit(&mut substitutions)
             .expect("Get output from generic backend");
         assert_eq!(output.stdout, b"I have 4096 mb of ram\n");
     }
@@ -129,12 +136,29 @@ mod tests {
         let config = Config::load_from_file("configs/generic_simple.toml")
             .expect("Load from example config");
         let backend = &config.backends[1];
-        let mut replacements = HashMap::new();
-        replacements.insert("ram".to_string(), 2.to_string());
+        let mut substitutions = HashMap::new();
+        substitutions.insert("ram".to_string(), 2.to_string());
 
         let output = backend
-            .submit(&mut replacements, "${", "}")
+            .submit(&mut substitutions)
             .expect("Get output from generic backend");
         assert_eq!(output.stdout, b"I have 2 mb of ram\n");
+    }
+
+    #[test]
+    fn lsf_example() {
+        let config = Config::load_from_file("configs/lsf.toml").expect("Load from example config");
+        let backend = &config.backends[0];
+        let mut substitutions = HashMap::new();
+        substitutions.extend(backend.runtime_attrs.clone().unwrap());
+
+        match &backend.kind {
+            super::BackendType::Generic(generic) => {
+                let command_str = generic.command.clone();
+                let subbed = super::substitute_placeholders(&command_str, &substitutions);
+                assert_eq!(subbed, "    bsub -q compbio -n 1 -g crankshaft -R \"rusage[mem=~{memory}] span[hosts=1]\" -cwd ~{cwd} -o ~{cwd}/execution/stdout.lsf -e ~{cwd}/execution/stderr.lsf /usr/bin/env bash ~{script}\n");
+            }
+            _ => panic!("Expected generic backend"),
+        }
     }
 }
